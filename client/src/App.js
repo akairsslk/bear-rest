a34e4cc811be128a5cf91ceaefddde4385f8eb5a
akairsslk <akairsslk@gmail.com>
2018-04-28 00:29:45 +0700

    react-redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,23 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducers'
-import { fetchBear} from './actions';
-
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-
-
-//import reducers from separate file
-let store = createStoreWithMiddleware(reducers)
+import { connect } from 'react-redux';
 
 
 class App extends Component {
-	constructor()
+	constructor(props)
 	{
-		super();
-		this.state = {data: []};
+		super(props);
 	}
 
-	updateMessage() {
-		this.setState({message: "my friend (from changed state) !"});
-	}
-
-
-	componentDidMount(){
-		store.subscribe(() => {
-			//need to give keys so the combineReducer can select the correct reducer
-			this.setState({data: store.getState().bear})
-		})
-		store.dispatch(fetchBear());
-	}
-	
 	render() {
-		let bears = this.state.data;
+		let bears = this.props.bear;
 		return (
 			<div> {bears.map(bear => <div key={bear.id}>{bear.name}</div>)}</div>
 			);
 	}
 }
 
-export default App;
+let mapStateToProps = (state) => (
+	{bear: state.bear}
+)
+
+export default connect(mapStateToProps)(App);

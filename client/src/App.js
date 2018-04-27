f1142cdaf9eee96ace84cfb1b4c66f3bece88f6d
akairsslk <akairsslk@gmail.com>
2018-04-27 22:59:24 +0700

    list bear with fail safe

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+	constructor()
+	{
+		super();
+		this.state = {data: []};
+	}
+
+	updateMessage() {
+		this.setState({message: "my friend (from changed state) !"});
+	}
+
+	getBear() {
+		try {
+			return axios.get('http://localhost:8000/api/bears')
+		} catch (error){
+			console.error(error);
+		}
+	}
+
+	componentDidMount(){
+		this.getBear().then(result => {
+			this.setState({data: result.data})
+		})
+	}
+	
   render() {
+  	let bears = this.state.data;
     return (
-      <div>Hello Mississippi</div>
+      <div> {bears.map(bear => <div key={bear.id}>{bear.name}</div>)}</div>
     );
   }
 }

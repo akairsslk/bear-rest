bb6f9112cda4ccaa22af506591ef08e551d6bdc0
akairsslk <akairsslk@gmail.com>
2018-04-27 23:58:27 +0700

    basic redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,42 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
+const getBear = () => {
+	try {
+		return axios.get('http://localhost:8000/api/bears')
+	} catch (error){
+		console.error(error);
+	}
+}
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+//reducer specify how state changed in respond to action
+//Ex a pure function that take the previous state and an action then give the next state
+//action are payload of information send from app to store, is store only 
+//source of information, send using dispatch(store.dispatch() or use libraly)
+let reducer = function(state = [], action){
+	if(action){
+		if(action.type == 'FETCH_BEAR'){
+			//action type specify how to react
+			return action.payload
+		}
+	}
+	return state
+}
+let store = createStoreWithMiddleware(reducer)
+
+//action creator is function that create action, can see from dispatch
+//in the function which is what use to send action to store
+let fetchBearActionCreator = function(){
+	return (dispatch) => {
+		getBear()
+		.then(result => {
+			dispatch({type: 'FETCH_BEAR', payload: result.data})
+		})
+	}
+}
 class App extends Component {
 	constructor()
 	{
@@ -12,26 +48,20 @@ class App extends Component {
 		this.setState({message: "my friend (from changed state) !"});
 	}
 
-	getBear() {
-		try {
-			return axios.get('http://localhost:8000/api/bears')
-		} catch (error){
-			console.error(error);
-		}
-	}
 
 	componentDidMount(){
-		this.getBear().then(result => {
-			this.setState({data: result.data})
+		store.subscribe(() => {
+			this.setState({data: store.getState()})
 		})
+		store.dispatch(fetchBearActionCreator());
 	}
 	
-  render() {
-  	let bears = this.state.data;
-    return (
-      <div> {bears.map(bear => <div key={bear.id}>{bear.name}</div>)}</div>
-    );
-  }
+	render() {
+		let bears = this.state.data;
+		return (
+			<div> {bears.map(bear => <div key={bear.id}>{bear.name}</div>)}</div>
+			);
+	}
 }
 
 export default App;
